14f3d5f430a4d75f2d85c21f2dd113858a225a42
Abdullah Zeynel <0abdullahzeynel0@gmail.com>
2026-03-04 00:15:50 +0300

    🚀 Special Update: GAZISIBER v3.0 hidden subpage (/v3)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import Soon from './components/Soon';
+import SoonV3 from './components/SoonV3';
 
 function App() {
     return (
         <ThemeProvider>
             <div className="min-h-screen">
-                <Soon />
+                <Routes>
+                    <Route path="/" element={<Soon />} />
+                    <Route path="/v3" element={<SoonV3 />} />
+                </Routes>
             </div>
         </ThemeProvider>
     );
